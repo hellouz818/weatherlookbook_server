b4758aff79ee2fd7fbf9a679bbea07181c8ee368
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-11-07 11:52:30 +0900

    Edit Board app (Search Filter)

--- weatherlookbook/boardapp/views.py
+++ weatherlookbook/boardapp/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from rest_framework import viewsets # vieset import
-from .serializers import BoardSerializer # 생성한 serializer import
+from rest_framework import viewsets
+from .serializers import BoardSerializer 
 from .models import Board
-from django.http import JsonResponse
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
 
-class BoardViewSet(viewsets.ModelViewSet): # ModelViewSet 활용
+class BoardViewSet(viewsets.ModelViewSet): 
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    filter_backends = [SearchFilter]
+    search_fields = ['content', 'date']
