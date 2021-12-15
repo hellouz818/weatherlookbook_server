4d5b1509c0bda36578a198437b006ec2741f45dc
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-12-15 18:22:18 +0900

    Edit Comment App

--- weatherlookbook/commentapp/urls.py
+++ weatherlookbook/commentapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("comment/<int:bid>/", views.commentcreate, name="coomentcreate"),
+    path("comment/<int:bid>/", views.commentcreate, name="commentcreate"),
 ]
