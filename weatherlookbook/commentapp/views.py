2d2f3048e31efa758d823a161d0d25af428e3d56
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-12-15 17:19:18 +0900

    Edit Comment App

--- weatherlookbook/commentapp/views.py
+++ weatherlookbook/commentapp/views.py
@@ -1,3 +1,24 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
-
+from django.http.response import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from boardapp.models import Board
+from accountapp.models import User
+from .models import Comment
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated, ))
+@csrf_exempt
+def commentcreate(request, bid):
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.board = Board.objects.get(bid = bid)
+        comment.text = request.POST['text']
+        comment.save()
+        return JsonResponse({"msg":"commentcreate success"})
+    else :
+        return JsonResponse({"msg":"comment create error"})
